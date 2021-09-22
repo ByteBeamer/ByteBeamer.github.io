56e745d058905570c7537560f276c5a3f956f6e7
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-09-21 20:41:15 -0500

    Update download.php

--- downloads/download.php
+++ downloads/download.php
@@ -1,4 +1,4 @@
-<? // Code to track the file using PHP, whether that means storing data in a database, saving to a log, or emailing you. I'd use a DB, like so:
+<?php // Code to track the file using PHP, whether that means storing data in a database, saving to a log, or emailing you. I'd use a DB, like so:
 
    // Prep the vars
    $file_id = $_GET['file_id']; // You should sanitize this first.
@@ -14,3 +14,4 @@
    header('Content-type: application/pdf');
    header('Content-Disposition: attachment; filename='.$file_id.'.pdf); // or whatever the file name is
    readfile($file_path);
+?>
