a55f75f5b42bb4067a87c5689331050d0cfc8b74
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-10-15 13:11:39 -0500

    Update main.js

--- game/platformer/js/main.js
+++ game/platformer/js/main.js
@@ -1,3 +1,15 @@
+PlayState = {};
+
+PlayState.preload = function () {
+    this.game.load.image('background', 'images/background.png');
+};
+
+PlayState.create = function () {
+    this.game.add.image(0, 0, 'background');
+};
+
 window.onload = function () {
     let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+    game.state.add('play', PlayState);
+    game.state.start('play');
 };
