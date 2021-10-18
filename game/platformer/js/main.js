460638bf059968a3cb616d19233eb92838387495
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-10-18 08:11:12 -0500

    Update main.js

--- game/platformer/js/main.js
+++ game/platformer/js/main.js
@@ -34,6 +34,11 @@ Hero.prototype.jump = function () {
     return canJump;
 };
 
+Hero.prototype.bounce = function () {
+    const BOUNCE_SPEED = 200;
+    this.body.velocity.y = -BOUNCE_SPEED;
+};
+
 function Spider(game, x, y) {
     Phaser.Sprite.call(this, game, x, y, 'spider');
 
@@ -64,6 +69,14 @@ Spider.prototype.update = function () {
     else if (this.body.touching.left || this.body.blocked.left) {
         this.body.velocity.x = Spider.SPEED; // turn right
     }
+};
+
+Spider.prototype.die = function () {
+    this.body.enable = false;
+
+    this.animations.play('die').onComplete.addOnce(function () {
+        this.kill();
+    }, this);
 };
 
 // =============================================================================
@@ -107,13 +120,15 @@ PlayState.preload = function () {
 
     this.game.load.audio('sfx:jump', 'audio/jump.wav');
     this.game.load.audio('sfx:coin', 'audio/coin.wav');
+    this.game.load.audio('sfx:stomp', 'audio/stomp.wav');
 };
 
 PlayState.create = function () {
     // create sound entities
     this.sfx = {
         jump: this.game.add.audio('sfx:jump'),
-        coin: this.game.add.audio('sfx:coin')
+        coin: this.game.add.audio('sfx:coin'),
+        stomp: this.game.add.audio('sfx:stomp')
     };
 
     this.game.add.image(0, 0, 'background');
@@ -131,6 +146,20 @@ PlayState._handleCollisions = function () {
     this.game.physics.arcade.collide(this.hero, this.platforms);
     this.game.physics.arcade.overlap(this.hero, this.coins, this._onHeroVsCoin,
         null, this);
+    this.game.physics.arcade.overlap(this.hero, this.spiders,
+        this._onHeroVsEnemy, null, this);
+};
+
+PlayState._onHeroVsEnemy = function (hero, enemy) {
+    if (hero.body.velocity.y > 0) { // kill enemies when hero is falling
+        hero.bounce();
+        enemy.die();
+        this.sfx.stomp.play();
+    }
+    else { // game over -> restart the game
+        this.sfx.stomp.play();
+        this.game.state.restart();
+    }
 };
 
 PlayState._onHeroVsCoin = function (hero, coin) {
