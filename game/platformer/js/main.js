c0994e8f87d29905258a89b88a387a9c14e6cfdf
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-11-19 12:49:26 -0600

    Update main.js

--- game/platformer/js/main.js
+++ game/platformer/js/main.js
@@ -81,46 +81,6 @@ Hero.prototype._getAnimationName = function () {
 //
 // boss Spider (enemy)
 //
-function BossSpider(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'boss_spiders');
-
-    // anchor
-    this.anchor.set(0.5);
-    // animation
-    this.animations.add('crawl', [0, 1, 2], 8, true); // 8fps, looped
-    this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
-    this.animations.play('crawl');
-
-    // physic properties
-    this.game.physics.enable(this);
-    this.body.collideWorldBounds = true;
-    this.body.velocity.x = BossSpider.SPEED;
-}
-
-BossSpider.SPEED = 200;
-
-// inherit from Phaser.Sprite
-BossSpider.prototype = Object.create(Phaser.Sprite.prototype);
-BossSpider.prototype.constructor = BossSpider;
-
-BossSpider.prototype.update = function () {
-    // check against walls and reverse direction if necessary
-    if (this.body.touching.right || this.body.blocked.right) {
-        this.body.velocity.x = -BossSpider.SPEED; // turn left
-    }
-    else if (this.body.touching.left || this.body.blocked.left) {
-        this.body.velocity.x = BossSpider.SPEED; // turn right
-    }
-};
-
-BossSpider.prototype.die = function () {
-    this.body.enable = false;
-
-    this.animations.play('die').onComplete.addOnce(function () {
-        this.kill();
-    }, this);
-};
-
 //spider
 function Spider(game, x, y) {
     Phaser.Spider.call(this, game, x, y, 'spider');
