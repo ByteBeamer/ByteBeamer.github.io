41bc2ca4f771c981998bf66ad43e35714ae337f5
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-11-19 08:16:18 -0600

    Update main.js

--- game/platformer/js/main.js
+++ game/platformer/js/main.js
@@ -79,10 +79,51 @@ Hero.prototype._getAnimationName = function () {
 };
 
 //
-// Spider (enemy)
+// boss Spider (enemy)
 //
+function BossSpider(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'boss_spiders');
+
+    // anchor
+    this.anchor.set(0.5);
+    // animation
+    this.animations.add('crawl', [0, 1, 2], 8, true); // 8fps, looped
+    this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
+    this.animations.play('crawl');
+
+    // physic properties
+    this.game.physics.enable(this);
+    this.body.collideWorldBounds = true;
+    this.body.velocity.x = BossSpider.SPEED;
+}
+
+BossSpider.SPEED = 200;
+
+// inherit from Phaser.Sprite
+BossSpider.prototype = Object.create(Phaser.Sprite.prototype);
+BossSpider.prototype.constructor = BossSpider;
+
+BossSpider.prototype.update = function () {
+    // check against walls and reverse direction if necessary
+    if (this.body.touching.right || this.body.blocked.right) {
+        this.body.velocity.x = -BossSpider.SPEED; // turn left
+    }
+    else if (this.body.touching.left || this.body.blocked.left) {
+        this.body.velocity.x = BossSpider.SPEED; // turn right
+    }
+};
+
+BossSpider.prototype.die = function () {
+    this.body.enable = false;
+
+    this.animations.play('die').onComplete.addOnce(function () {
+        this.kill();
+    }, this);
+};
+
+//spider
 function Spider(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'spider');
+    Phaser.Spider.call(this, game, x, y, 'spider');
 
     // anchor
     this.anchor.set(0.5);
@@ -96,6 +137,8 @@ function Spider(game, x, y) {
     this.body.collideWorldBounds = true;
     this.body.velocity.x = Spider.SPEED;
 }
+
+//SPIDERS
 
 Spider.SPEED = 100;
 
@@ -128,7 +171,7 @@ Spider.prototype.die = function () {
 
 PlayState = {};
 
-const LEVEL_COUNT = 7;
+const LEVEL_COUNT = 8;
 
 PlayState.init = function (data) {
     this.game.renderer.renderSession.roundPixels = true;
@@ -159,6 +202,7 @@ PlayState.preload = function () {
     this.game.load.json('level:4', 'data/level04.json');
     this.game.load.json('level:5', 'data/level05.json');
     this.game.load.json('level:6', 'data/level06.json');
+    this.game.load.json('level:7', 'data/level07.json');
 
     this.game.load.image('font:numbers', 'images/numbers.png');
 
