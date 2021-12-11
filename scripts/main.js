bcc9242af72f6285a8fed30e25de4d0cce454031
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-12-11 15:27:46 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,1 @@
-window.onscroll = function() {fade1()};
 
-function fade1() {
-  if (document.body.scrollTop > 1000 | document.documentElement.scrollTop > 1000) {
-    document.getElementById("banner").hidden = true;
-    document.getElementById("cards").hidden = false;
-  } else {
-    document.getElementById("banner").hidden = false;
-    document.getElementById("cards").hidden = true;
-  }
-}
