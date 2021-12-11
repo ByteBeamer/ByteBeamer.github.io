ebb81a9f56f1867d9116a3b63fc6be56b323b718
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-12-11 15:17:27 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,11 @@
-var header = document.getElementById('header');
+window.onscroll = function() {fade1()};
 
-function fadeOutOnScroll(element) {
-  if (!element) {
-    return;
-  }
-  
-  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-  var elementHeight = element.offsetHeight;
-  var scrollTop = document.documentElement.scrollTop;
-  
-  var opacity = 1;
-  
-  if (scrollTop > distanceToTop) {
-    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-  }
-  
-  if (opacity >= 0) {
-    element.style.opacity = opacity;
+function fade1() {
+  if (document.body.scrollTop > 1000 | document.documentElement.scrollTop > 1000) {
+    document.getElementById("banner").diplay = "blank";
+    document.getElementById("cards").display = "block";
+  } else {
+    document.getElementById("banner").diplay = "block";
+    document.getElementById("cards").display = "blank";
   }
 }
-
-function scrollHandler() {
-  fadeOutOnScroll(header);
-}
-
-window.addEventListener('scroll', scrollHandler);
