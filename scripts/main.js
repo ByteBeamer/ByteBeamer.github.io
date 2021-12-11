a93e249f9e4f84329529bec99178b20c8a7a0f69
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-12-11 15:17:55 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ window.onscroll = function() {fade1()};
 function fade1() {
   if (document.body.scrollTop > 1000 | document.documentElement.scrollTop > 1000) {
     document.getElementById("banner").diplay = "blank";
-    document.getElementById("cards").display = "block";
+    document.getElementById("cards").display = "grid";
   } else {
     document.getElementById("banner").diplay = "block";
     document.getElementById("cards").display = "blank";
