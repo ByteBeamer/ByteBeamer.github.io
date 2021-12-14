0e87a818cebf70a2720f7b7d84c0efcc574acd02
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-12-14 15:14:10 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 var section = document.getElementsByClassName('section')[1];
-var height  = document.getElementsByClassName('cards')[0].offsetHeight;
-section.setAttribute("style","width:" + height);
+var height  = document.getElementsByClassName('cards')[0].style.height;
+section.setAttribute("style","height:" + height);
