1e578edc18eb43b08fbeff47d44cd243eb2bcbc1
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-12-24 21:43:18 -0600

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,3 +4,12 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+
+const buttons = document.querySelectorAll("a");
+buttons.forEach(btn => {
+  btn.addEventListener("click", function(e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop
+  })
+})
