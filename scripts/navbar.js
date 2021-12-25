3c6e0ab73445fa26848567cdf9906736c1517750
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-12-25 07:20:26 -0600

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -6,7 +6,7 @@ toggleButton.addEventListener('click', () => {
 })
 
 
-const buttons = document.getElementsByClassName("navbar");
+const buttons = document.querySelectorAll("a");
 buttons.forEach(btn => {
   btn.addEventListener("click", function(e) {
     let x = e.clientX - e.target.offsetLeft;
