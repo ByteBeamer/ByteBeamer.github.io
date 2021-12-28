7764dfe2e51ddc177ae854c2c3537c9e32ca0726
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2021-12-28 11:58:50 -0600

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -12,8 +12,16 @@ buttons.forEach(btn => {
     /*let x = e.clientX - e.target.offsetLeft;
     let y = e.clientY - e.target.offsetTop;*/
     
-    let x = e.clientX;
-    let y = e.clientY;
+    let x, y;
+    
+    if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+        x = touch.pageX;
+        y = touch.pageY;
+    } else if (e.type == 'mousedown' || e.type == 'mouseup' || e.type == 'mousemove' || e.type == 'mouseover'|| e.type=='mouseout' || e.type=='mouseenter' || e.type=='mouseleave') {
+        x = e.clientX;
+        y = e.clientY;
+    }
     
     let ripples = document.createElement("span");
     ripples.style.left = '${x}px'
