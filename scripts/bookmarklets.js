ca6b9741c3d76a8d9d7389bc00434cb1e559a4af
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-01-10 15:39:03 -0600

    Update bookmarklets.js

--- scripts/bookmarklets.js
+++ scripts/bookmarklets.js
@@ -1,2 +1,18 @@
-var favorites = document.getElementsByClassname("favorite");
+var favorites = document.getElementsByClassName("favorite");
+var cards = document.getElementsByClassName("card");
 
+if (select == "All") {
+  for (var i=0, max=cards.length; i < max; i++) {
+    cards.style.visibility = "shown";
+  }
+}
+if (select == "Favorites") {
+  for (var i=0, max=cards.length; i < max; i++) {
+       if (cards.classList.contains('favorite')) {
+         favorites[i].style.visibility = "shown";
+       }
+       if (!cards.classList.contains('favorite')) {
+         cards[i].style.visibility = "hidden";
+       }
+  }
+}
