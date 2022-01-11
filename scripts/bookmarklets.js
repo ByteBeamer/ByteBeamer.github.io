f74b3b7d2840c6014a46f86621b2518f9a409760
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-01-11 09:15:11 -0600

    Update bookmarklets.js

--- scripts/bookmarklets.js
+++ scripts/bookmarklets.js
@@ -6,34 +6,17 @@ var value = select.options[select.selectedIndex].value;
 select.onchange=update();
 
 function update() {
-  for (var i=0; i <= cards.length; i++) {
+  
+  Array.prototype.forEach.call(cards, function(card) {
     if (value == "All") {
-      cards[i].style.visibility = "visible";
+      card.style.visibility = "visible";
     } else if (value == "Favorites") {
-      if (cards[i].classList.contains('favorite')) {
-         cards[i].style.visibility = "visible";
+      if (card.classList.contains('favorite')) {
+         card.style.visibility = "visible";
       }
-      else if (!cards[i].classList.contains('favorite')) {
-         cards[i].style.visibility = "hidden";
+      else if (!card.classList.contains('favorite')) {
+         card.style.visibility = "hidden";
       }
     }
-  }
-}
-
-function changeCards() {
-  if (value == "All") {
-  for (var i=0; i < cards.length; i++) {
-    cards[i].style.visibility = "shown";
-  }
-}
-if (value == "Favorites") {
-  for (var i=0; i < cards.length; i++) {
-       if (cards.classList.contains('favorite')) {
-         favorites[i].style.visibility = "shown";
-       }
-       if (!cards.classList.contains('favorite')) {
-         cards[i].style.visibility = "hidden";
-       }
-  }
-}
+  });
 }
