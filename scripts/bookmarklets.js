5230db52c07e8adfbbe4b2bd9f0ae426a8b17e0a
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-01-11 09:29:21 -0600

    Update bookmarklets.js

--- scripts/bookmarklets.js
+++ scripts/bookmarklets.js
@@ -3,9 +3,23 @@ var cards = document.body.getElementByClassName("cards")[0].getElementsByClassNa
 var select = document.body.getElementById('card_filter');
 var value = select.options[select.selectedIndex].value;
 
-select.onchange=update();
+select.onchange = function () {
+  Array.prototype.forEach.call(cards, function(card) {
+    if (value == "1") {
+      card.style.visibility = "visible";
+    } 
+    if (value == "2") {
+      if (card.classList.contains('favorite')) {
+         card.style.visibility = "visible";
+      }
+      if (!card.classList.contains('favorite')) {
+         card.style.visibility = "hidden";
+      }
+    }
+  });
+};
 
-function update() {
+function updateCards() {
   Array.prototype.forEach.call(cards, function(card) {
     if (value == "1") {
       card.style.visibility = "visible";
