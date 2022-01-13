9b2fbc72f64b422ebf30e5dcfd6ab041fdaee381
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-01-12 20:44:24 -0600

    Update bookmarklets.js

--- scripts/bookmarklets.js
+++ scripts/bookmarklets.js
@@ -1,20 +1,25 @@
-var cards = document.getElementByClassName("cards")[0].getElementsByClassName("card");
+var cards = document.getElementsByClassName("card");
 
-var select = document.getElementsByClassName("select")[0].getElementByClassName("dropdown-container")[0].getElementById('card_filter');
+var select = document.getElementById('card_filter');
 var value = select.options[select.selectedIndex].value;
 
 select.onchange = function () {
-  Array.prototype.forEach.call(cards, function(card) {
-    if (value == "1") {
-      card.style.visibility = 'visible';
-    } 
-    if (value == "2") {
-      if (card.classList.contains('favorite')) {
-         card.style.visibility = 'visible';
-      }
-      if (!card.classList.contains('favorite')) {
-         card.style.visibility = 'hidden";
+  Array.from(document.getElementsByClassName(cards)).forEach(
+      function (card) {
+        if (value == "1") {
+        card.style.visibility = 'visible';
+      } 
+      if (value == "2") {
+        if (card.classList.contains('favorite')) {
+          card.style.visibility = 'visible';
+        }
+        if (!card.classList.contains('favorite')) {
+          card.style.visibility = 'hidden";
+        }
       }
     }
+  );
+  Array.prototype.forEach.call(cards, function(card) {
+    
   });
 };
