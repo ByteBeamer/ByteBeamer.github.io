3662056e591c29b33581cc3ce83f2941408e4198
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-01-12 21:25:06 -0600

    Update bookmarklets.js

--- scripts/bookmarklets.js
+++ scripts/bookmarklets.js
@@ -8,11 +8,11 @@ select.onchange = function () {
   value = select.options[select.selectedIndex].value;
   Array.prototype.forEach.call(cards, function(card) {
       if (value == "1") {
-        card.style.display = "inherit";
+        card.style.display = "block";
       } 
       if (value == "2") {
         if (card.classList.contains('favorite')) {
-          card.style.display = "inherit";
+          card.style.display = "block";
         }
         else {
           card.style.display = "none";
