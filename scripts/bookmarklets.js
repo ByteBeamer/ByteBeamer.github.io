20d15894946dc20219be548c8e24b3fcf64af0bd
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-01-12 21:08:44 -0600

    Update bookmarklets.js

--- scripts/bookmarklets.js
+++ scripts/bookmarklets.js
@@ -3,17 +3,18 @@ var cards = document.getElementsByClassName("card");
 var select = document.getElementById('card_filter');
 var value = select.options[select.selectedIndex].value;
 
-select.onclick = function () {
+select.onchange = function () {
+  alert("changed! Value: " + value);
   Array.prototype.forEach.call(cards, function(card) {
       if (value == "1") {
-        card.style.display = "block";
+        card.style.visibility = "visible";
       } 
       if (value == "2") {
         if (card.classList.contains('favorite')) {
-          card.style.display = "block";
+          card.style.visibility = "visible";
         }
         else {
-          card.style.display = "none";
+          card.style.visibility = "hidden";
         }
       }
   });
