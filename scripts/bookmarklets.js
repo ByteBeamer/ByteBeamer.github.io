2ae6046d886327ee7c361d4caf6f6dcb56c9a5ef
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-01-12 20:50:42 -0600

    Update bookmarklets.js

--- scripts/bookmarklets.js
+++ scripts/bookmarklets.js
@@ -4,22 +4,17 @@ var select = document.getElementById('card_filter');
 var value = select.options[select.selectedIndex].value;
 
 select.onclick = function () {
-  Array.from(document.getElementsByClassName(cards)).forEach(
-      function (card) {
-        if (value == "1") {
+  Array.prototype.forEach.call(cards, function(card) {
+      if (value == "1") {
         card.style.visibility = 'visible';
       } 
       if (value == "2") {
         if (card.classList.contains('favorite')) {
-          card.style.visibility = 'visible';
+          card.style.visibility = 'hidden';
         }
-        if (!card.classList.contains('favorite')) {
+        else {
           card.style.visibility = "hidden";
         }
       }
-    }
-  );
-  Array.prototype.forEach.call(cards, function(card) {
-    
   });
 };
