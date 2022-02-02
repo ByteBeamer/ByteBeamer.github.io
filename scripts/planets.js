45a70a9e06bc3d1cc4efb1f65d2723302e904258
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-02-02 09:49:52 -0600

    Create planets.js

---
+++ scripts/planets.js
@@ -0,0 +1,10 @@
+window.onscroll = function () {
+    scrollRotate();
+};
+
+function scrollRotate() {
+    let image = document.getElementById("reload");
+    image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
+
+
+}
