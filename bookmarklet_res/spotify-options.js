f0c2aefe5584cd99124530f283b85b85667abbf6
ByteBeamer <81110386+ByteBeamer@users.noreply.github.com>
2022-08-16 14:16:53 -0500

    Update spotify-options.js

--- bookmarklet_res/spotify-options.js
+++ bookmarklet_res/spotify-options.js
@@ -3,7 +3,7 @@ var win = window.open("","",true);
 var title = document.querySelectorAll("span")[11].innerText;
 var artist = document.querySelectorAll("span")[12].innerText;
 
-win.document.write(`<h1>${title}</h1><br><h2>By: ${artist}</h2>`)
+win.document.write(`<h1>${title}</h1><h2>By: ${artist}</h2>`)
 
 function tick() {
   
